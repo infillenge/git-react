d0f8585308a0714fbd0c29ffd3f140922c69733c
infillenge <development@infillenge.com>
2020-12-11 08:44:35 +0900

    first

---
+++ code-git/git-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{width:'100vw', height:'100vh', display:'flex', alignItems:'center', justifyContent:'center'}}>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default App;
